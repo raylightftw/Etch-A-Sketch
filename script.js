e8bbd91dac3facd1ccc68190c0ede776167306ca
raylightftw <rayadithya@gmail.com>
2022-08-09 09:53:20 -0700

    Add Clear button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var jk=4;
+
 var grids=[];
 for(var i=0;i<16;i++)
 {   grids[i]=document.createElement('div');
@@ -6,13 +6,14 @@ for(var i=0;i<16;i++)
     document.querySelector('.Grid').appendChild(grids[i]);
 }
 greatHover();
+var newGrid=4;
 var button=document.querySelector('.newsize')
 button.addEventListener('click',()=>{askUser()})
 var newgrids=[];
 var grid=document.querySelector('.Grid')
 function askUser()
 {
-    var newGrid=prompt("Enter new grid size!",4);
+    newGrid=prompt("Enter new grid size!",4);
     while (grid.childElementCount!=0)
     {
         grid.innerHTML="";
@@ -40,5 +41,22 @@ grid_array.forEach(div => div.addEventListener('mouseover',function() {
 } ) )
 }
 
+clear=document.querySelector('.clear')
+clear.addEventListener('click',()=>{greatClear()})
 
 
+function greatClear()
+{   
+    console.log(newGrid);
+    grid.innerHTML="";
+    for(var i=0;i<Math.pow(newGrid,2);i++)
+    {   grid.style.gridTemplateColumns = `repeat(${newGrid}, 1fr)`;
+        grid.style.gridTemplateRows = `repeat(${newGrid}, 1fr)` ;
+        newgrids[i]=document.createElement('div');
+        newgrids[i].classList.add('Grids');
+        document.querySelector('.Grid').appendChild(newgrids[i]);
+        greatHover()
+    }
+}
+
+//https://imgur.com/bkyienb.png
